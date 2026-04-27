f3df61b6361dcf4a58992275749cdeb677498711
Sundeep <sundeepm1@gmail.com>
2026-04-27 11:23:50 +0530

    updated api workflow files

--- backend/api/Program.cs
+++ backend/api/Program.cs
@@ -5,7 +5,6 @@ using Microsoft.Data.SqlClient;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
